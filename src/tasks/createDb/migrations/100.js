f9dd9ff4e668318aecd04d92b9c620041778b35d
Dhika Rizky <deka_r@outlook.com>
2020-02-25 02:21:34 +0700

    Updated migration script

--- src/tasks/createDb/migrations/100.js
+++ src/tasks/createDb/migrations/100.js
@@ -14,7 +14,7 @@ export default function run () {
       table.string('image', 255)
     })
     .createTable('bookAuthor', table => {
-      table.integer('book').notNullable().unsigned().references('id').inTable('books')
+      table.integer('book').notNullable().unsigned().references('id').inTable('book')
       table.integer('author').notNullable().unsigned().references('id').inTable('author')
     })
     .createTable('role', table => {
@@ -29,5 +29,21 @@ export default function run () {
       table.string('email', 255).unique().index().notNullable()
       table.integer('role').notNullable().unsigned().references('id').inTable('role')
       table.dateTime('registeredDate').notNullable()
+    })
+    .createTable('permission', table => {
+      table.increments('id').primary()
+      table.string('name').notNullable()
+    })
+    .createTable('rolePermission', table => {
+      table.integer('role').notNullable().unsigned().references('id').inTable('role')
+      table.integer('permission').notNullable().unsigned().references('id').inTable('permission')
+    })
+    .createTable('borrowing', table => {
+      table.increments('id').primary()
+      table.dateTime('borrowingTime').notNullable()
+      table.dateTime('returningTime')
+      table.integer('book').notNullable().unsigned().references('id').inTable('book')
+      table.integer('borrower').notNullable().unsigned().references('id').inTable('user')
+      table.integer('staff').notNullable().unsigned().references('id').inTable('user')
     })
 }
